de4778d1d56409318f947f5873c19b8e0a04fd6f
Raphael.Shin <108261108+shinseogyong@users.noreply.github.com>
2023-07-03 12:24:14 +0900

    Add files via upload
    자바 기초 1강

---
+++ 07.03/Ex01_for.java
@@ -0,0 +1,30 @@
+
+public class Ex01_for {
+
+	public static void main(String[] args) {
+		//제어문 - 반복문(for, while, do ~ while)
+		//-----------------------------------------------
+		int member = 3;
+		for (int i = 1; i <= member; i++) { // i는 지역변수
+			System.out.println("*");
+		}
+		System.out.println("=============================");
+		
+		for (int j = 0; j <= 10; j+=2) {
+			System.out.println("* = "+ j +"번째 별");
+		}
+		System.out.println("=============================");
+		
+		//-----------------------------------------------
+		
+		System.out.println("----------------1부터 5까지 출력");
+		
+		//반복인자 값을 사용해서 출력
+		for(int k = 1; k <= 5; k++) {
+			System.out.println(k + " = " + k + "번째 숫자");
+		}
+		System.out.println("=============================");
+		
+	}
+
+}
