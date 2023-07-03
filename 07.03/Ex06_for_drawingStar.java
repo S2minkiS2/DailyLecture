d07f785d8660af88f902b0c2aa46dfbf326ef200
Raphael.Shin <108261108+shinseogyong@users.noreply.github.com>
2023-07-03 18:14:56 +0900

    Add files via upload
    수정

--- 07.03/Ex06_for_drawingStar.java
+++ 07.03/Ex06_for_drawingStar.java
@@ -6,11 +6,11 @@ public class Ex06_for_drawingStar {
 
 		System.out.println("-------for문 두번 적용--------");
 		
-		for (int i = 1; i <= 3; i++) {
-			for(int j = 1; j <= 5; j++) {
+		for (int line = 1; line <= 4; line++) { //4개의 줄에 반복
+			for(int star = 1; star <= 6; star++) { //한 줄에 별 6개 찍기 반복
 				System.out.print("*");
 			}
-			System.out.println();
+			System.out.println(); //줄마다 별 찍고 line숫자 넘어가는 것
 		}
 		
 		System.out.println("------- 그리기--------");
