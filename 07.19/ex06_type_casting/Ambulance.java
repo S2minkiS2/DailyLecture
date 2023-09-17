7359f9577dc60d811c3867f0fdd2245bf63b3c2d
Raphael.Shin <108261108+shinseogyong@users.noreply.github.com>
2023-09-17 22:17:21 +0900

    Delete 07.19 directory

--- 07.19/ex06_type_casting/Ambulance.java
+++
@@ -1,8 +0,0 @@
-package com.mystudy.ex06_type_casting;
-
-public class Ambulance extends Car {
-	
-	void siren() {
-		System.out.println(">> 응급환자 이송 사이렌~");
-	}
-}
--- 07.19/poly1_extends/Ambulance.java
+++
@@ -1,8 +0,0 @@
-package com.mystudy.poly1_extends;
-
-public class Ambulance extends Car {
-	
-	void siren() {
-		System.out.println(">> 응급환자 이송 사이렌~");
-	}
-}
