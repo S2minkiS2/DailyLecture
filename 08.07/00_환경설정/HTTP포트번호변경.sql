77e2766aeb43501ff05eb9d2f6866c8cb82ab3ba
Raphael.Shin <108261108+shinseogyong@users.noreply.github.com>
2023-09-17 22:18:39 +0900

    Delete 08.07 directory

--- 08.07/00_환경설정/HTTP포트번호변경.sql
+++
@@ -1,9 +0,0 @@
--- SQL 에서 한 줄 주석 부호(--)
--- HTTP 포트 변경 : 8080 -> 8090
-SELECT DBMS_XDB.getHttpPort() FROM dual;
-SELECT DBMS_XDB.GETHTTPPORT() FROM DUAL;
-
--- HTTP 포트 8090 으로 변경
-exec dbms_xdb.setHttpPort(8090);
-
-SELECT DBMS_XDB.GETHTTPPORT() FROM DUAL;
